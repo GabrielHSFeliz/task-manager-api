fa2dee02eb642bcc4033030fcc1202ec6dad12d7
Gabriel Feliz <feliz.gabriel785@gmail.com>
2026-07-17 11:28:51 -0300

    Fase 4: cadastro de usuario com hash de senha

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from database import get_db
+from models import User
+from schemas import UserCreate, UserResponse
+from security import hash_password
 
 app = FastAPI(title="Task Manager API")
 
 @app.get("/")
 def read_root():
     return {"message": "Task Manager API rodando!"}
+
+@app.post("/register", response_model=UserResponse)
+def register(user: UserCreate, db: Session = Depends(get_db)):
+    # Verifica se o usuário já existe
+    existing_user = db.query(User).filter(User.email == user.email).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Email já registrado")
+
+    # Cria um novo usuário
+    hashed_password = hash_password(user.password)
+    new_user = User(name=user.name, email=user.email, hashed_password=hashed_password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
